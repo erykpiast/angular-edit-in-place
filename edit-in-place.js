f6a49946b62da6193ef2c477197bfc805d6d2b4c
Eryk Napierała <eryk.piast@gmail.com>
2014-01-22 23:11:42 +0100

    main directive file added

---
+++ edit-in-place.js
@@ -0,0 +1,55 @@
+angular.module('editInPlace', [ ])
+.directive( 'editInPlace', function($compile, $parse) {
+        return {
+            restrict: 'A',
+            scope: { model: '=editInPlace' },
+            template: '<span ng-bind="model"></span><input ng-model="model" type="text" class="edit-in-place__input" />',
+            compile: function(tElement, tAttrs) {
+                var $input = tElement.children('.edit-in-place__input'),
+                    inputId = 'edit-in-place-' + ~~(Math.random() * Date.now());
+                
+                $input.attr('id', inputId);
+                
+                if(tAttrs.label) {
+                    var $label = angular.element('<label for="' + inputId + '" class="edit-in-place__label"></label>');
+                    
+                    $label.insertBefore($input);
+                }
+                
+                return function(scope, $element, attrs) {
+                    scope.editing = false;
+    
+                    var $input = $element.children('.edit-in-place__input');
+                    $input
+                        .hide()
+                        .on('blur', function() {
+                            $input.hide();
+
+                            scope.editing = false;
+                        });
+                        
+                    if(attrs.inputAttrs) {
+                        angular.forEach($parse(attrs.inputAttrs)(scope.$parent), function(attrValue, attrName) {
+                            $input.attr(attrName, attrValue);
+                        });
+                        
+                        $compile($input[0])(scope, function(scope, cloned) {
+                           // $element.append(cloned);
+                        });
+                        
+                    }
+    
+                    $element
+                        .addClass('edit-in-place')
+                        .on('click', function(e) {
+                            e.preventDefault();
+    
+                            $input.show();
+                            $input.focus();
+    
+                            scope.editing = true;
+                        });
+                };
+            }
+        };
+    });
